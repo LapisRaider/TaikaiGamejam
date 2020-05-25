d63a0a75081baa69c7a3f0caf99c323646dc0200
Angie <173123N@mymail.nyp.edu.sg>
2020-05-25 17:59:25 +0800

    - some test sprites added - Added sprite editor and tilemap

---
+++ TaikaiGameJam/Assets/Scripts/System/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class SoundManager : SingletonBase<SoundManager>
+{
+    public Sound[] m_SoundClipList;
+
+    public void Awake()
+    {
+        foreach(Sound sound in m_SoundClipList)
+        {
+            sound.m_Source = gameObject.AddComponent<AudioSource>();
+            sound.m_Source.clip = sound.m_Clip;
+
+            sound.m_Source.volume = sound.m_Volume;
+            sound.m_Source.pitch = sound.m_Pitch;
+            sound.m_Source.loop = sound.m_Loop;
+            sound.m_Source.spatialBlend = sound.m_HearingBaseOnDist;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound playingSound = Array.Find(m_SoundClipList, sound => sound.m_Name == name);
+
+        if (playingSound == null)
+            return;
+
+        playingSound.m_Source.Play();
+    }
+}

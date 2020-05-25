e49c2dec7c04c427b94382c76112492d2d199748
Angie <173123N@mymail.nyp.edu.sg>
2020-05-25 19:18:46 +0800

    - created map manager - Camera can move and clamp accordingly to the size of the tilemap

---
+++ TaikaiGameJam/Assets/Scripts/Entities/NPCs/Volunteers.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volunteers : MonoBehaviour
+{
+    //will walk around the map
+    //will check from mapmanager whether the space is free to plant
+    //plant tree
+    //move on
+
+    //different states, IDLE mode, where theyll just wander about and search for areas for trees
+    //plant trees mode, go to the destination and plant a tree
+    //back to idle mode
+    public Vector2 m_MinMaxSpeed = new Vector2(0.1f,1.0f);
+
+
+    public enum States
+    {
+        IDLE, //move ard freely
+        MOVE_TO_FREE_SPACE, //move to a space to plant tree
+        PLANT_TREE, //plant a tree down
+        CHASE //for chasing away evil business people
+    }
+
+    CharacterController m_CharacterController;
+    Animator m_Animator;
+
+    States m_CurrentState = States.IDLE;
+    Vector2 m_MoveDir = Vector2.zero;
+    float m_Speed = 0.0f;
+
+    public void Awake()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+        m_Animator = GetComponent<Animator>();
+
+        m_Speed = Random.Range(m_MinMaxSpeed.x, m_MinMaxSpeed.y);
+    }
+
+    public void Update()
+    {
+        switch(m_CurrentState)
+        {
+            case States.IDLE:
+                UpdateIdleState();
+                break;
+            case States.MOVE_TO_FREE_SPACE:
+                break;
+            case States.PLANT_TREE:
+                break;
+            case States.CHASE:
+                break;
+        }
+    }
+
+    public void ChangeState(States newState)
+    {
+        //check current state and exit
+        switch (m_CurrentState)
+        {
+            case States.IDLE:
+                ExitIdleState();
+                break;
+            case States.MOVE_TO_FREE_SPACE:
+                ExitMoveToFreeSpaceState();
+                break;
+            case States.PLANT_TREE:
+                ExitPlantTreeState();
+                break;
+            case States.CHASE:
+                ExitChaseState();
+                break;
+        }
+
+        //check new state and enter
+        switch (newState)
+        {
+            case States.IDLE:
+                EnterIdleState();
+                break;
+            case States.MOVE_TO_FREE_SPACE:
+                EnterMoveToFreeSpaceState();
+                break;
+            case States.PLANT_TREE:
+                EnterPlantTreeState();
+                break;
+            case States.CHASE:
+                EnterChaseState();
+                break;
+        }
+
+        m_CurrentState = newState;
+    }
+
+    #region IdleState
+    public void EnterIdleState()
+    {
+        //change animation accordingly
+    }
+
+    public void UpdateIdleState()
+    {
+        //make NPC wander around the map, every few seconds will change direction
+
+    }
+
+    public void ExitIdleState()
+    {
+
+    }
+    #endregion
+
+    #region MoveToFreeSpaceState
+    public void EnterMoveToFreeSpaceState()
+    {
+
+    }
+
+    public void UpdateMoveToFreeSpaceState()
+    {
+
+    }
+
+    public void ExitMoveToFreeSpaceState()
+    {
+
+    }
+    #endregion
+
+    #region PlantTreeState
+    public void EnterPlantTreeState()
+    {
+
+    }
+
+    public void UpdatePlantTreeState()
+    {
+
+    }
+
+    public void ExitPlantTreeState()
+    {
+
+    }
+    #endregion
+
+    #region ChaseState
+    public void EnterChaseState()
+    {
+
+    }
+
+    public void UpdateChaseState()
+    {
+
+    }
+
+    public void ExitChaseState()
+    {
+
+    }
+    #endregion
+}
---
+++ TaikaiGameJam/Assets/Scripts/System/PlayerInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Camera")]
+    public float m_CameraMoveSpeed = 10.0f;
+    public Bounds m_CameraBoundary;
+    Camera m_MainCamera;
+
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+        m_CameraBoundary = MapManager.Instance.m_MapBoundary;
+        float cameraHeight = 2.0f * m_MainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * m_MainCamera.aspect;
+
+        m_CameraBoundary.min = new Vector3(m_CameraBoundary.min.x + cameraWidth / 2.0f, m_CameraBoundary.min.y + m_MainCamera.orthographicSize, m_CameraBoundary.min.z);
+        m_CameraBoundary.max = new Vector3(m_CameraBoundary.max.x - cameraWidth / 2.0f, m_CameraBoundary.max.y - m_MainCamera.orthographicSize, m_CameraBoundary.max.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCamera();
+    }
+
+    #region Camera
+    public void UpdateCamera()
+    {
+        Vector3 dir = CameraInput();
+        Vector3 newCameraTargetPos = m_MainCamera.transform.position + (dir * m_CameraMoveSpeed * Time.deltaTime);
+        newCameraTargetPos = new Vector3(Mathf.Clamp(newCameraTargetPos.x, m_CameraBoundary.min.x, m_CameraBoundary.max.x), Mathf.Clamp(newCameraTargetPos.y, m_CameraBoundary.min.y, m_CameraBoundary.max.y), newCameraTargetPos.z);
+
+        Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, newCameraTargetPos, 1.0f);
+    }
+
+    public Vector2 CameraInput()
+    {
+        Vector2 dir = Vector2.zero;
+
+        if (Input.GetKey("left"))
+            dir.x = -1.0f;
+        else if (Input.GetKey("right"))
+            dir.x = 1.0f;
+
+        if (Input.GetKey("up"))
+            dir.y = 1.0f;
+        else if (Input.GetKey("down"))
+            dir.y = -1.0f;
+
+        return dir;
+    }
+    #endregion
+}

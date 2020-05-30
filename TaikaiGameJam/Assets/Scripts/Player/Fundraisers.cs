beb4ca1664d9d8670481a9fc34b4f6a1d308249e
Angie <173123N@mymail.nyp.edu.sg>
2020-05-31 01:04:42 +0800

    - fundraiser UI is done

--- TaikaiGameJam/Assets/Scripts/Player/Fundraisers.cs
+++ TaikaiGameJam/Assets/Scripts/Player/Fundraisers.cs
@@ -15,13 +15,36 @@ public class Fundraisers : MonoBehaviour
 
     [Header("UI stuff")]
     public TextMeshProUGUI[] m_NumbersTextUI;
-    public int[] m_SignificantNumberPlace;
+    [HideInInspector] public int[] m_SignificantNumberPlace;
+    public TextMeshProUGUI m_PercentageText;
 
     [HideInInspector] public int m_CurrentAmtSpent = 0;
+    [HideInInspector] public float m_ChanceOfSuccess = 0.0f;
+
+    public void OnEnable()
+    {
+        foreach(TextMeshProUGUI text in m_NumbersTextUI)
+        {
+            text.text = (0).ToString();
+        }
+
+        for (int i =0; i < m_SignificantNumberPlace.Length; ++i)
+        {
+            m_SignificantNumberPlace[i] = 0;
+        }
+
+        GetCurrentSuccessRate();
+        
+        if (m_PercentageText != null)
+            m_PercentageText.text = "Chance of success based on popularity: " + (m_ChanceOfSuccess * 100.0f) + "%";
+    }
 
     #region UI stuff
     public void IncreaseNumber(int significantNumberPlace)
     {
+        if (significantNumberPlace < 0 || significantNumberPlace >= m_SignificantNumberPlace.Length)
+            return;
+
         ++m_SignificantNumberPlace[significantNumberPlace];
 
         if (m_SignificantNumberPlace[significantNumberPlace] > 9)
@@ -32,6 +55,9 @@ public class Fundraisers : MonoBehaviour
 
     public void DecreaseNumber(int significantNumberPlace)
     {
+        if (significantNumberPlace < 0 || significantNumberPlace >= m_SignificantNumberPlace.Length)
+            return;
+
         --m_SignificantNumberPlace[significantNumberPlace];
 
         if (m_SignificantNumberPlace[significantNumberPlace] < 0)
@@ -46,32 +72,56 @@ public class Fundraisers : MonoBehaviour
     }
     #endregion
 
-    //TODO:: IF PLAYER AMT IS NEGATIVE, WARN PLAYER BEFORE THEY CAN SPEND
+    public void ConfirmPayment()
+    {
+        SetCurrentAmtSpent();
+
+        Money money = GameStats.Instance.m_Money;
+        if (money == null)
+            return;
+
+        if (money.m_CurrMoney - m_CurrentAmtSpent <= 0)
+        {
+            //TODO:: IF PLAYER AMT IS NEGATIVE, WARN PLAYER BEFORE THEY CAN SPEND
+
+        }
+        else
+        {
+            CheckSuccess();
+        }
+    }
+
     public void SetCurrentAmtSpent()
     {
         m_CurrentAmtSpent = 0;
         for (int i = 0; i < m_SignificantNumberPlace.Length; ++i)
         {
-            m_CurrentAmtSpent += (int)(m_SignificantNumberPlace[i] * Mathf.Pow(10, i));
+            m_CurrentAmtSpent += (int)(m_SignificantNumberPlace[i] * Mathf.Pow(10, (m_SignificantNumberPlace.Length - i - 1)));
         }
     }
 
-    public void CheckSuccess()
+    public void GetCurrentSuccessRate()
     {
-        //deduct first
-        GameStats.Instance.m_Money.ReduceMoney(m_CurrentAmtSpent, false);
-
         //get the current popularity number
         int currentPopularity = GameStats.Instance.m_Popularity.m_CurrentPopularity;
         //divide by the number per percentage increase
         float m_PercentageIncrease = (currentPopularity / m_PopularityIncreaseRate) / 100.0f;
 
         //add to the min successpercentage
-        float m_CurrentSuccessRate = m_PercentageIncrease + m_MinMaxSuccessPercentage.x;
-        m_CurrentSuccessRate = Mathf.Clamp(m_CurrentSuccessRate, m_MinMaxSuccessPercentage.x, m_MinMaxSuccessPercentage.y);
+        m_ChanceOfSuccess = m_PercentageIncrease + m_MinMaxSuccessPercentage.x;
+        m_ChanceOfSuccess = Mathf.Clamp(m_ChanceOfSuccess, m_MinMaxSuccessPercentage.x, m_MinMaxSuccessPercentage.y);
+    }
+
+    public void CheckSuccess()
+    {
+        if (GameStats.Instance.m_Money == null)
+            return;
+
+        //deduct first
+        GameStats.Instance.m_Money.ReduceMoney(m_CurrentAmtSpent, false);
 
         //randomize float and check if its within the range
-        if (Random.Range(0.0f,1.0f) <= m_CurrentSuccessRate) //pass
+        if (Random.Range(0.0f,1.0f) <= m_ChanceOfSuccess) //pass
         {
             //get back the amount of money spent and more
             int moneyReturn = (int)(m_CurrentAmtSpent + m_CurrentAmtSpent * Random.Range(m_MinMaxSuccessIncreasePercentageAmt.x, m_MinMaxSuccessIncreasePercentageAmt.y));

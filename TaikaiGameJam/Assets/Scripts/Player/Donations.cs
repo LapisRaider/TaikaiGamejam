4a39fb24ebef95d997ac806cdfe5110ec7c473e4
Angie <173123N@mymail.nyp.edu.sg>
2020-06-01 03:57:15 +0800

    - increase donation

--- TaikaiGameJam/Assets/Scripts/Player/Donations.cs
+++ TaikaiGameJam/Assets/Scripts/Player/Donations.cs
@@ -6,6 +6,7 @@ public class Donations
     [Header("Earn amount")]
     public float m_AmtPerTree = 0.5f;
     public float m_AmtPerPlant = 0.05f;
+    public float m_MonthlyDefaultFunding = 100;
 
     //a certain percentage u might gain or lose
     public Vector2 m_MinMaxDonationPercentage = new Vector2(0.8f, 1.2f);
@@ -18,6 +19,7 @@ public class Donations
         float amt = m_AmtPerTree * currentTreeNumber + currentPlantNumber * m_AmtPerPlant;
         amt = amt * (1.0f + ((int)popularityCount / m_NumberOfDonorPerPercentage));
         amt = amt * Random.Range(m_MinMaxDonationPercentage.x, m_MinMaxDonationPercentage.y);
+        amt += m_MonthlyDefaultFunding;
 
         return Mathf.RoundToInt(amt);
     }

9cbb6d310ca122d59e7c70f1861775561c0dc50a
Angie <173123N@mymail.nyp.edu.sg>
2020-06-01 01:18:41 +0800

    - main menu

---
+++ TaikaiGameJam/Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
